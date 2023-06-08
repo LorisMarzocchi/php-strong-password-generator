4a81e41e343a0ef656fd3d939165276a310bd1d6
LorisMarzocchi <ml77182a@gmail.com>
2023-06-08 17:53:44 +0200

    addStyle

--- index.php
+++ index.php
@@ -43,23 +43,82 @@ $password = randomString($length);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-strong-password-generator</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
 </head>
 
 <body>
-    <div>
-        <form action="" method="GET">
-            <label for="str_leng">Lunghezza password:</label>
-            <input type="number" name="str_leng" id="str_leng" value="<?php echo $length; ?>">
-            <input type="radio" name="" id="">
-            <input type="checkbox" name="" id="">
-            <input type="checkbox" name="" id="">
-            <input type="checkbox" name="" id="">
-            <button type="submit">Submit</button>
-            <a href="">Annulla</a>
+    <div class="container containerBig">
+        <div class="text-center">
+
+            <h1 style="color: #808B99;">Strong Password Generator</h1>
+            <h2 style="color: white;">Genera una password sicura</h2>
+        </div>
+
+        <div class="alert" role="alert">
+            Nessun Parametro valido inserito
+        </div>
+        <form action="" method="GET" class="w-75 m-auto d-flex justify-content-between">
+            <div class="mt-4 btn_control">
+                <label for="exampleInputEmail1" class="form-label">Lunghezza Password:</label>
+                <div>
+                    <button type="submit" class="btn btn-primary me-3">Submit</button>
+                    <a class="btn btn-secondary resetBtn" href="/php-strong-password-generator">Annulla</a>
+                </div>
+
+            </div>
+            <div class="mt-4 ">
+
+                <div>
+                    <input input type="number" name="str_leng" id="str_leng" value="<?php echo $length; ?>" max="20">
+
+                </div>
+
+                <!-- radio -->
+                <div class="form-check mt-2">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        Si
+                    </label>
+                </div>
+                <div class="form-check mt-2">
+
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                        No
+                    </label>
+                </div>
+
+                <!-- check -->
+
+                <div class="form-check mt-2">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Lettere
+                    </label>
+                </div>
+                <div class="form-check mt-2">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Numeri
+                    </label>
+                </div>
+                <div class="form-check mt-2">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Simboli
+                    </label>
+                </div>
+            </div>
+
         </form>
-        <h1 style="font-size: 2rem;"><?php echo $password; ?></h1>
+
+    </div>
+    <div>
+
+
+        <p class="mt-4" style="font-size: 2rem; color: white;">La tua Password è: <?php echo $password; ?></p>
     </div>
 </body>
 
